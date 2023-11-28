5e63759b639f17519e2a7d5fe91dab048b432bfc
ebadwebdev9231 <kebad9231@gmail.com>
2023-11-28 15:29:41 +0500

    Create a generic data fetching hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import clientApi from "../services/client-api";
+import { CanceledError } from "axios";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    clientApi
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setLoading(false);
+        setData(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export default useData;
