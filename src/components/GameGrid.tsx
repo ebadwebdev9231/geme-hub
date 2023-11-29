b8f1a074d8e8f59d718272a0c69292b56062ce16
ebadwebdev9231 <kebad9231@gmail.com>
2023-11-29 15:51:36 +0500

    Highlight the selected genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,13 +11,13 @@ interface Props {
 
 const GameGrid = ({ selectedGenre }: Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
         padding="10px"
         spacing={4}
       >
