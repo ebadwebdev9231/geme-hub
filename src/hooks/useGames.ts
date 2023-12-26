9629761a953a989cfc329141fa8378c80e34583c
ebadwebdev9231 <kebad9231@gmail.com>
2023-12-26 16:37:45 +0500

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatform";
 import ClientApi from "../services/client-api";
@@ -31,6 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000,
   });
 
 export default useGames;
