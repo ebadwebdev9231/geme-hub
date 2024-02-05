fd8fea3d15493341d1742b9b90ce22d2ffa912a1
ebadwebdev9231 <kebad9231@gmail.com>
2024-02-05 15:45:19 +0500

    Build Game Trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import ClientApi from "../services/client-api";
+import { Trailer } from "../entities/Trailer";
+
+const useTrailers = (gameId: number) => {
+  const clientApi = new ClientApi<Trailer>(`games/${gameId}/movies`);
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: clientApi.getAll,
+  });
+};
+
+export default useTrailers;
